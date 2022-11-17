515ab682307796b661815342c75dab71b1472c99
Srigadgeter <srigadgets@gmail.com>
2022-11-17 17:53:06 +0530

    i18n locales

---
+++ src/i18n/locales/ar.js
@@ -0,0 +1,7 @@
+export default {
+  translation: {
+    hello: 'مرحبا بالعالم!',
+    'read docs': 'اقرأ المستندات لاكتشاف ما يجب فعله بعد ذلك:',
+    'change lang': 'تغيير اللغة',
+  },
+};
---
+++ src/i18n/locales/en.js
@@ -0,0 +1,7 @@
+export default {
+  translation: {
+    hello: 'Hello World!',
+    'read docs': 'Read the docs to discover what to do next:',
+    'change lang': 'Change Language',
+  },
+};
